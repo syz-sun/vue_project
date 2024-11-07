4880932b30a2408ec70753909398d2bb94379133
syz-sun <syz929648400@gmail.com>
2024-11-07 23:23:17 +0800

    全部提交

---
+++ src/components/login/Login.vue
@@ -0,0 +1,8 @@
+<template>
+    登录页面
+</template>
+<script>
+    export default{
+        name:"UserLogin"
+    }
+</script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
+import Router from './tools/Router'
+import Store from './tools/Storage'
+
+import ElementPlus from 'element-plus'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(ElementPlus).use(Router).use(Store)
+app.mount('#app')
+                                                                                                                                                                                                                                                    
